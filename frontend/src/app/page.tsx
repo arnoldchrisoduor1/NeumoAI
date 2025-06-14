1a19d61456a071847d2038d0378b1ff672d783bb
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-06-14 23:16:49 +0300

    deployments

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
         
         {/* Medium particles */}
         <div className="absolute inset-0 opacity-30">
-          {[...Array(60)].map((_, i) => (
+          {[...Array(20)].map((_, i) => (
             <div
               key={`medium-${i}`}
               className="absolute rounded-full bg-[#8a2be2]"
@@ -53,7 +53,7 @@ export default function Home() {
         
         {/* Small fast-moving particles */}
         <div className="absolute inset-0 opacity-40">
-          {[...Array(100)].map((_, i) => (
+          {[...Array(50)].map((_, i) => (
             <div
               key={`small-${i}`}
               className="absolute rounded-full bg-[#9370db]"
@@ -72,7 +72,7 @@ export default function Home() {
         
         {/* Sparkles */}
         <div className="absolute inset-0 opacity-70">
-          {[...Array(30)].map((_, i) => (
+          {[...Array(20)].map((_, i) => (
             <div
               key={`sparkle-${i}`}
               className="absolute bg-white"
